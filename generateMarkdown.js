7810630c76eca0ec15fd6c0cd0b24e0af8ba1e9f
Tino Nguyen <101163927+Verreauxx@users.noreply.github.com>
2022-06-07 21:07:32 -0700

    Add files via upload

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -15,7 +15,6 @@ function renderLicenseLink(license) {
 function renderLicenseSection(license) {
   if (license !== 'None') {
     return `## License
-
 This project is licensed under the ${license} license.`;
   }
   return '';
@@ -24,57 +23,37 @@ This project is licensed under the ${license} license.`;
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
-
 ## Description
-
 ${data.description}
-
 ## Table of Contents 
-
 * [Installation](#installation)
-
 * [Usage](#usage)
 ${renderLicenseLink(data.license)}
 * [Contributing](#contributing)
-
 * [Tests](#tests)
-
 * [Questions](#questions)
-
 ## Installation
-
 To install necessary dependencies, run the following command:
-
 \`\`\`
 ${data.installation}
 \`\`\`
-
 ## Usage
-
 ${data.usage}
-
 ${renderLicenseSection(data.license)}
   
 ## Contributing
-
 ${data.contributing}
-
 ## Tests
-
 To run tests, run the following command:
-
 \`\`\`
 ${data.test}
 \`\`\`
-
 ## Questions
-
-If you have any questions about the repo, open an issue or contact me directly at ${
+For more info or inquiries, email directly at ${
     data.email
   }. You can find more of my work at [${data.github}](https://github.com/${
     data.github
   }/).
-
 `;
 }
 
